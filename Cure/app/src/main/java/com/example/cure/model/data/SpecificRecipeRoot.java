8a79d009d34dfb6d8487699988d615bbef06f983
Ali Malla <alimal@student.chalmers.se>
2022-04-26 16:15:33 +0200

    create a class to save the response from specific recipe API

---
+++ Cure/app/src/main/java/com/example/cure/model/data/SpecificRecipeRoot.java
@@ -0,0 +1,12 @@
+package com.example.cure.model.data;
+
+public class SpecificRecipeRoot {
+    private Recipe recipe;
+
+    public SpecificRecipeRoot(Recipe recipe){
+        this.recipe = recipe;
+    }
+    public Recipe getRecipe() {
+        return recipe;
+    }
+}
