9a723e46698c0cd327431b45d3eb3a316c417839
Ali Malla <alimal@student.chalmers.se>
2022-04-29 03:37:25 +0200

    create a new variable "uri"

--- Cure/app/src/main/java/com/example/cure/model/data/Recipe.java
+++ Cure/app/src/main/java/com/example/cure/model/data/Recipe.java
@@ -4,6 +4,7 @@ public class Recipe {
 
     private final String label;
     private final String image;
+    private final String uri;
     private final float totalWeight;
     private final float calories;
     private final float yield;
@@ -17,12 +18,27 @@ public class Recipe {
     private RecipeImages images;
     private TotalNutrients totalNutrients;
 
-    public Recipe(String label, String image, float totalWeight, float calories, float yield, String[] mealType,
+    public Recipe(String label, String image, String uri, float totalWeight, float calories, float yield, String[] mealType, String[] dishType, String[] cuisineType, String[] dietLabels, String[] healthLabels) {
+        this.label = label;
+        this.image = image;
+        this.uri = uri;
+        this.totalWeight = totalWeight;
+        this.calories = calories;
+        this.yield = yield;
+        this.mealType = mealType;
+        this.dishType = dishType;
+        this.cuisineType = cuisineType;
+        this.dietLabels = dietLabels;
+        this.healthLabels = healthLabels;
+    }
+
+    public Recipe(String label, String image, String uri, float totalWeight, float calories, float yield, String[] mealType,
                   String[] dishType, String[] cuisineType, String[] dietLabels, String[] healthLabels,
                   Digest[] digests, Ingredient[] ingredients, RecipeImages images,
                   TotalNutrients totalNutrients) {
         this.label = label;
         this.image = image;
+        this.uri = uri;
         this.totalWeight = totalWeight;
         this.calories = calories;
         this.yield = yield;
@@ -91,5 +107,9 @@ public class Recipe {
 
     public TotalNutrients getTotalNutrients() {
         return totalNutrients;
+    }
+
+    public String getUri() {
+        return uri;
     }
 }
