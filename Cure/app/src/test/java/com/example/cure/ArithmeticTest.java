9ff561550a5b728e50e5b7720084c58afb494990
Ali Malla <alimal@student.chalmers.se>
2022-04-30 14:29:51 +0200

    create a class to test arithmetic class

---
+++ Cure/app/src/test/java/com/example/cure/ArithmeticTest.java
@@ -0,0 +1,23 @@
+package com.example.cure;
+
+import org.junit.Test;
+
+public class ArithmeticTest {
+
+    @Test
+    public void calculateTotalCaloriesTest(){
+    }
+
+    @Test
+    public void calculateTotalFatTest(){
+    }
+
+    @Test
+    public void calculateTotalCarbsTest(){
+    }
+
+    @Test
+    public void calculateTotalProteinTest(){
+    }
+
+}
