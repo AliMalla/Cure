de4215506cff7b204e5ed32b7c82a148fa3ac5d8
Ali <alialk@student.chalmers.se>
2022-05-06 14:14:54 +0200

    Adding logic/methods to viewmodel

--- Cure/app/src/main/java/com/example/cure/ui/dashboard/DashboardViewModel.java
+++ Cure/app/src/main/java/com/example/cure/ui/dashboard/DashboardViewModel.java
@@ -4,9 +4,26 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.cure.model.data.Hit;
+import com.example.cure.model.data.Recipe;
+import com.example.cure.model.data.Root;
+import com.example.cure.model.data.SpecificRecipeRoot;
+import com.example.cure.model.server.api.APIConnection;
+import com.example.cure.model.server.api.OnResponseListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class DashboardViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    public MutableLiveData<List<Root>> roots = new MutableLiveData<>();
+
+    private List<MainRecipeItem> items = new ArrayList<>();
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
@@ -16,4 +33,30 @@ public class DashboardViewModel extends ViewModel {
     public LiveData<String> getText() {
         return mText;
     }
+
+    private void fetchData(){
+        APIConnection.getRootModel("Chicken", new OnResponseListener() {
+            @Override
+            public void recipeByIdFetched(SpecificRecipeRoot sr) {
+
+            }
+
+            @Override
+            public void recipesByQueryFetched(Root r) {
+
+                for (Hit hit : r.getHits()) {
+                    Recipe rec = hit.getRecipe();
+                    items.add(new MainRecipeItem(rec.getLabel(), rec.getImage(), (int)rec.getCalories()+" kcal",
+                            "Fat    "+(int)rec.getTotalNutrients().getFat().getQuantity() + " " + rec.getTotalNutrients().getFat().getUnit(), "Protein    "+(int)rec.getTotalNutrients().getProtein().getQuantity() +" "+ rec.getTotalNutrients().getProtein().getUnit(),
+                            "Carbs    "+(int)rec.getTotalNutrients().getCarbs().getQuantity() + " " +rec.getTotalNutrients().getCarbs().getUnit(), (int)rec.getTotalTime() + " minuter"));
+                }
+            }
+        });
+    }
+
+    public List<MainRecipeItem> getItems() {
+        fetchData();
+        return items;
+    }
+
 }
