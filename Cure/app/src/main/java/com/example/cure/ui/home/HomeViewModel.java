d2aa6a4959e98b42bb93431883f8ff9c8a37fe0b
Ali <alialk@student.chalmers.se>
2022-05-06 14:21:34 +0200

    Add logic in viewmodel to recive the data from API and send it to view

--- Cure/app/src/main/java/com/example/cure/ui/home/HomeViewModel.java
+++ Cure/app/src/main/java/com/example/cure/ui/home/HomeViewModel.java
@@ -1,42 +1,38 @@
 package com.example.cure.ui.home;
 
 import android.content.Context;
-import android.content.SearchRecentSuggestionsProvider;
-import android.util.Log;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.cure.model.data.Hit;
 import com.example.cure.model.data.Recipe;
+import com.example.cure.model.data.Root;
 import com.example.cure.model.data.SpecificRecipeRoot;
 import com.example.cure.model.other.Arithmetic;
-import com.example.cure.model.other.DataConverter;
 import com.example.cure.model.server.api.APIConnection;
+import com.example.cure.model.server.api.OnResponseListener;
 import com.example.cure.model.server.database.Repository;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
     private Repository rep;
-    private List<Item> items;
+    private List<DailyRecipeItem> dailyRecipeItems = new ArrayList<>();
     private final Arithmetic arithmetic;
-
+    private List<SpecificRecipeRoot> list = new ArrayList<>();
+    private List<Recipe> recipes = new ArrayList<>(); //test
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
         //rep = new Repository(context);
-        //this.items = getItems(recipes);
+        //this.dailyRecipeItems = getItems(recipes);
         this.arithmetic = new Arithmetic();
     }
 
@@ -44,50 +40,75 @@ public class HomeViewModel extends ViewModel {
         this.rep = new Repository(context);
         this.arithmetic = new Arithmetic();
     }
-    /*
-    private List<Item> getItems(List<Recipe> recipes){
-        final List<Item> items = new ArrayList<>();
 
-        for (Recipe recipe : recipes) {
-            String name = recipe.getLabel();
-            String image = recipe.getImage();
-            int calories = (int)recipe.getCalories();
-            Item item = new Item(name, image, calories, Item.Type.LUNCH);
-            items.add(item);
-        }
-
-        return items;
+    public List<SpecificRecipeRoot> getList() {
+        return list;
     }
 
- */
 
     public void deleteItem(String id, Calendar date) {
         rep.deleteRecipe(id, date);
     }
 
     public double getDailyCalories(Calendar date) {
-        List<Recipe> recipes = rep.getRecipes(date);
         return arithmetic.calculateTotalCalories(recipes);
     }
 
     public double getDailyProtein(Calendar date) {
-        List<Recipe> recipes = rep.getRecipes(date);
         return arithmetic.calculateTotalProtein(recipes);
     }
 
     public double getDailyCarbs(Calendar date) {
-        List<Recipe> recipes = rep.getRecipes(date);
         return arithmetic.calculateTotalCarbs(recipes);
     }
 
     public double getDailyFat(Calendar date) {
-        List<Recipe> recipes = rep.getRecipes(date);
         return arithmetic.calculateTotalFat(recipes);
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+
+
+
+    public List<String> recipeIdList(Calendar date){
+        List<String> list = new ArrayList<>();
+        list.add("e7e22b5f9afdae010472f2084a76fd6c");
+        list.add("b66666d5c882ca199f43def8f1b8a03f");
+
+        return list;
+    }
+
+
+    private void fetchDailyRecipes(Calendar date) {
+        List<String> recipeIdList = recipeIdList(date);
+        for (String id : recipeIdList) {
+            APIConnection.getRecipeById(id, new OnResponseListener() {
+                @Override
+                public void recipeByIdFetched(SpecificRecipeRoot sr) {
+                    if (dailyRecipeItems.size() != recipeIdList.size()) {
+                        dailyRecipeItems.add(new DailyRecipeItem(id, sr.getRecipe().getLabel(),
+                                sr.getRecipe().getImage(), (int) sr.getRecipe().getCalories(), DailyRecipeItem.Type.LUNCH));
+                        recipes.add(sr.getRecipe());
+                    }
+                }
+
+                @Override
+                public void recipesByQueryFetched(Root r) {
+
+                }
+            });
+        }
+
+    }
+
+
+    public List<DailyRecipeItem> getDailyRecipeItems(Calendar date) {
+        fetchDailyRecipes(date);
+        return dailyRecipeItems;
+    }
+
 
 
 }
