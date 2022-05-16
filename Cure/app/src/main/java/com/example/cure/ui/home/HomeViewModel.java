2c51f9fb0e1d88019034873bef7aff4ddbf539e1
Ali <alialk@student.chalmers.se>
2022-05-16 20:09:51 +0200

    Make nutrient values calculated per person instead

--- Cure/app/src/main/java/com/example/cure/ui/home/HomeViewModel.java
+++ Cure/app/src/main/java/com/example/cure/ui/home/HomeViewModel.java
@@ -77,10 +77,11 @@ public class HomeViewModel extends ViewModel {
                 @Override
                 public void recipeByIdFetched(SpecificRecipeRoot sr) {
                     if (dailyRecipeItems.size() != recipeIdList.size()) {
-                        String [] dishTypes = sr.getRecipe().getDishType();
-                        dailyRecipeItems.add(new DailyRecipeItem(id, sr.getRecipe().getLabel(),
-                                sr.getRecipe().getImage(), (int) sr.getRecipe().getCalories(), dishTypes[0].toUpperCase(Locale.ROOT)));
-                        recipes.add(sr.getRecipe());
+                        Recipe recipe = sr.getRecipe();
+                        String [] dishTypes = recipe.getDishType();
+                        dailyRecipeItems.add(new DailyRecipeItem(id, recipe.getLabel(),
+                                recipe.getImage(), (int)(recipe.getCalories()/recipe.getYield()), dishTypes[0].toUpperCase(Locale.ROOT)));
+                        recipes.add(recipe);
                     }
                 }
 
